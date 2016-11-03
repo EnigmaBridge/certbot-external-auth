23d4af0961d555e47abd1fc90a09b50ff014f264
Dusan Klinec <dusan.klinec@gmail.com>
2016-11-04 00:51:45 +0100

    v0.0.6, classifiers added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.0.5'
+version = '0.0.6'
 
 install_requires = [
     'acme',
@@ -16,6 +16,27 @@ setup(
     author="Dusan Klinec",
     author_email='dusan@enigmabridge.com',
     license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'Environment :: Plugins',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+    ],
+
     install_requires=install_requires,
     packages=find_packages(),
     entry_points={
